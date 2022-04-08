c6152bc1196a00fa72f2bd0a1414a6731c06f4e0
{T.Moch} <mochida.tetsuya@insent.co.jp>
2022-04-08 17:12:38 +0900

    write_list_2d 開始行、列の指定をデバグ

--- hamadas.py
+++ hamadas.py
@@ -21,15 +21,24 @@ def write_list_2d(sheet, l_2d, start_row, start_col):
 target_dir = '\\\\192.168.24.27\\disk1\\New共通\\生産部\\品質保証\\05_生産\\02_生産管理\\02_工程管理\\測定値記録自動化\\濱田さんEXCEL\\'
 dst_file = '★基本測定データの判定.xlsm'
 
-Ver = 'ver_20211224'
-#   Ver = 'ver_20220311'
+#   Ver = 'ver_20211224'
+Ver = 'ver_20220311'
 
 if Ver == 'ver_20211224':
     ref_file = '■基本測定データの判定(20211224) - コピー.xlsm'
     h_sheet_name = '7ｻﾝﾌﾟﾙ3周(Z)'               #   ■基本測定データの判定ver2(20220311)対応
+    start_row_prt = 4
+    start_col_prt = 3
+    start_row_abs = 65
+    start_col_abs = 2
 elif Ver == 'ver_20220311':
     ref_file = '■基本測定データの判定ver2(20220311) - コピー.xlsm'
-    h_sheet_name = '7ｻﾝﾌﾟﾙ3周'               #   ■基本測定データの判定ver2(20220311)対応   
+    h_sheet_name = 'データ貼付け用'               #   ■基本測定データの判定ver2(20220311)対応  
+    start_row_prt = 1
+    start_col_prt = 17
+    start_row_abs = 1
+    start_col_abs = 2
+
 
 # FileSystemEventHandler の継承クラスを作成
 class FileChangeHandler(FileSystemEventHandler):
@@ -67,7 +76,7 @@ class FileChangeHandler(FileSystemEventHandler):
              h_sheet = hamadabook[h_sheet_name]
 
              #   prtデータを貼り付け（行、列　＝　４，３から）
-             write_list_2d(h_sheet, prt_values_ss, 4, 3)
+             write_list_2d(h_sheet, prt_values_ss, start_row=start_row_prt, start_col=start_col_prt)
 
              #   excelファイル保存
              hamadabook.save(target_dir + dst_file)
@@ -99,7 +108,7 @@ class FileChangeHandler(FileSystemEventHandler):
              h_sheet = hamadabook[h_sheet_name]
 
              #   absデータを貼り付け（行、列　＝　６５，２から）
-             write_list_2d(h_sheet, abs_values_ss, 65, 2)
+             write_list_2d(h_sheet, abs_values_ss, start_row=start_row_abs, start_col=start_col_abs)
 
              #   excelファイル保存
              hamadabook.save(target_dir + dst_file)
