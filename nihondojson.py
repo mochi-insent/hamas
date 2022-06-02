ae69012ebaa09258762826feb28ab4f46735ee4f
{T.Moch} <mochida.tetsuya@insent.co.jp>
2022-06-02 10:51:06 +0900

    ログ定義ファイルのjsonを作るために日本語unicode変換

--- nihondojson.py
+++ nihondojson.py
@@ -6,7 +6,7 @@ dict = {}
 dict["Email"] = "taro@example.com"
 dict["Active"] = True
 dict["CreatedDate"] = "2013-01-20T00:00:00Z"
-dict["Roles"] = ["ユーザー", "管理者", "\\\\192.168.24.27\\disk1\\New共通\\生産部\\品質保証\\05_生産\\02_生産管理\\02_工程管理\\測定値記録自動化\\試し\\ハートビート\\BG\\log_.txt"]
+dict["Roles"] = ["ユーザー", "管理者", "\\\\192.168.24.27\\disk1\\New共通\\生産部\\品質保証\\05_生産\\02_生産管理\\02_工程管理\\測定値記録自動化\\製膜秤量\\LOG\\hamadas_log_.txt"]
 
 # ensure_ascii "なし" で出力します
 with open("output_uniEscape.json", "w", encoding="utf-8" ) as outputFile:
